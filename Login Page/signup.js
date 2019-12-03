6518603db2067ac142e9fc23334f2b8f340a75f0
Calvin-Zikakis <cazi6864@colorado.edu>
2019-12-03 12:58:37 -0700

    couple quick changes

--- Login Page/signup.js
+++ Login Page/signup.js
@@ -58,12 +58,8 @@ function openModal() {
     }
     confirmMyInput.onkeyup = function() {
                 // Validate password and confirmPassword
-                if (confirmMyInput.value == myInput.value){
-                    var passEqualsConfPass = true;
-                }
-                else if(confirmMyInput.value != myInput.value){
-                    var passEqualsConfPass = false;
-                }
+                var passEqualsConfPass = (confirmMyInput.value == myInput.value)
+
                 if(passEqualsConfPass) { 
                     match.classList.remove("invalid"); 
                     match.classList.add("valid"); 
@@ -78,23 +74,16 @@ function openModal() {
 
 
 function enableButton(letter, capital, number, length, match) {
-    var button = document.getElementById('my_submit_button');
-    var amIgood = false;
-
-    if(match && length && number && capital && letter){
-        amIgood = true;
-    }
-
-    var condition = (amIgood); 
-    if(condition) {       
-        button.disabled = false;
-        }
-    else{
-        button.disabled = true;
-    }      
-    }    
+   // TODO: Clear this function for students to implement    
+   var button = document.getElementById('my_submit_button');
+   var condition = (letter.classList == "valid" && capital.classList == "valid" && number.classList == "valid" && length.classList == "valid" && match.classList == "valid"); // TODO: Replace false with the correct condition
+   
+   if(condition) {      
+           button.disabled = false;
+       }        
+   }
 
 
 function onClickFunction() {
-    alert("Hey! I'm all green! Well done.")
+    location.href = "../homepage/home.html";
 }
